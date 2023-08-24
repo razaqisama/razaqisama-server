df416189e0b229aa6ed8898d896f6b3d982bb550
razaqisama <razaqisama@gmail.com>
2023-08-25 02:02:56 +0700

    fix code style

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,15 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({
-      message: "Application Deployed",
-  })
-})
+    message: "Application Deployed",
+  });
+});
 
-router.get('/blogs', (_req, res) => {
-  res.send('/blog-post');
+router.get("/blogs", (_req, res) => {
+  res.send("/blog-post");
 });
 // Add more routes as needed
 
