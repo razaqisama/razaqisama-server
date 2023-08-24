2e5f21c265ff43c18988bb9df3f3e60fa9f81469
razaqisama <razaqisama@gmail.com>
2023-08-25 01:59:37 +0700

    add eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
-import 'dotenv/config'
-import express from 'express';
-import mongoose from 'mongoose';
-import { config } from './config';
-import blogRoutes from './routes';
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "./config";
+import blogRoutes from "./routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/', blogRoutes);
+app.use("/", blogRoutes);
 
-mongoose.connect(config.db.url || '').then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+mongoose
+  .connect(config.db.url || "")
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
   });
-}).catch(error => {
-  console.error('Error connecting to MongoDB:', error);
-});
--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,5 @@ export const config = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     url: process.env.DB_URL,
-  }
-}
+  },
+};
